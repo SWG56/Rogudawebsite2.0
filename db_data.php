eb15572912ce15a4baddf7d0f4ff4b750adbad91
SWG56 <vhuthu.munyai02@gmail.com>
2026-01-23 23:45:17 +0200

    php

--- db_data.php
+++ db_data.php
@@ -1,5 +1,6 @@
- 
 <?php
+declare(strict_types=1);
+
 $host = "localhost";
 $db   = "rogudat3b7y0_admissions";
 $user = "rogudat3b7y0_nyiko";
@@ -9,13 +10,9 @@ $charset = "utf8mb4";
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
+  PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+  PDO::ATTR_EMULATE_PREPARES   => false,
 ];
 
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (Exception $e) {
-    die("Database connection failed: " . $e->getMessage());
-}
+$pdo = new PDO($dsn, $user, $pass, $options);
